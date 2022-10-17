215515b3440dfc6ad45bcbfdc1fafe59942c30fe
kalamalahala <solo.driver.bob@gmail.com>
2022-10-17 17:37:22 -0400

    add js listerner

--- includes/settings/layouts/bulk-message.php
+++ includes/settings/layouts/bulk-message.php
@@ -10,7 +10,9 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 <form id="bulk-form" method="post" action="">
     <label for="phone-numbers">Phone Numbers</label>
-    <input id="phone-numbers" type="textarea" name="phone-numbers"></input>
+    <input id="phone-numbers" type="textarea"
+    rows="10"
+    cols="50" name="phone-numbers"></input>
     <label for="message">Message</label>
     <input id="message" type="textarea" name="message" value="Awesome, we will be conducting a virtual career briefing this Wednesday at 10 AM via Zoom. Please register, and we'll see you then! https://us02web.zoom.us/webinar/register/WN___mUcBTbRJiBptjaL2ZBGQ"></input>
     <input id="form-submit" type="submit" name="submit" value="Send"></input>
