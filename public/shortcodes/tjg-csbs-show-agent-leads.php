4e5c084c44ca46c83d67b01e8a143ec0f5ba2df0
kalamalahala <solo.driver.bob@gmail.com>
2022-10-18 14:06:12 -0400

    it returns false

--- public/shortcodes/tjg-csbs-show-agent-leads.php
+++ public/shortcodes/tjg-csbs-show-agent-leads.php
@@ -11,9 +11,7 @@ function get_candidate_layout()
     $user_id = $user->ID;
     $leads = new Common();
     $num_leads = $leads->get_candidates_assigned_to_user($user_id);
-    $num_leads = count($num_leads);
-    $has_leads = ($num_leads > 0) ? true : false;
-    if (!$has_leads) return '<p>You have no leads assigned to you.</p>';
+    if (!$num_leads) return '<p>You have no leads assigned to you.</p>';
     // include tjg-csbs-public-display.php
     ob_start();
     include plugin_dir_path(dirname(__FILE__)) . '/partials/tjg-csbs-public-display.php';
