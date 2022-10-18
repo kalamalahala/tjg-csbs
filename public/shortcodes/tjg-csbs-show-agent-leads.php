4f0d2805d31b7a05b05217ea16429d29413c5966
kalamalahala <solo.driver.bob@gmail.com>
2022-10-18 13:36:28 -0400

    guard clause for no assigned leads

--- public/shortcodes/tjg-csbs-show-agent-leads.php
+++ public/shortcodes/tjg-csbs-show-agent-leads.php
@@ -3,9 +3,17 @@
 /**
  * File containing methods related to the Agent Leads table
  */
+use Tjg_Csbs_Common as Common;
 
 function get_candidate_layout()
 {
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+    $leads = new Common();
+    $num_leads = $leads->get_candidates_assigned_to_user($user_id);
+    $num_leads = count($num_leads);
+    $has_leads = ($num_leads > 0) ? true : false;
+    if (!$has_leads) return '<p>You have no leads assigned to you.</p>';
     // include tjg-csbs-public-display.php
     ob_start();
     include plugin_dir_path(dirname(__FILE__)) . '/partials/tjg-csbs-public-display.php';
