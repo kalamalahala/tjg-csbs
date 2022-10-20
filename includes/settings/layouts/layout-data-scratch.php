3a50f1fc8abcfd4d1bd67ef91406b2a401077c79
kalamalahala <solo.driver.bob@gmail.com>
2022-10-20 18:01:34 -0400

    another method

--- includes/settings/layouts/layout-data-scratch.php
+++ includes/settings/layouts/layout-data-scratch.php
@@ -53,7 +53,11 @@ $user_id = get_current_user_id();
 
     // var dump a candidate by id using the Candidate object and candidate_object_test method
     $candidate = $common->candidate_object_test(1);
+    $oop_candidate = new Candidate(1);
+    
     var_dump($candidate);
+
+    var_dump($oop_candidate);
 
     ?>
 
