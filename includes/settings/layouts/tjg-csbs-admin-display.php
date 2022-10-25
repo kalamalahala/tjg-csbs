ef481844db7d3e393dcfc85c777e06c2322a4a9a
kalamalahala <solo.driver.bob@gmail.com>
2022-10-24 23:59:26 -0400

    holy shit type was already defined

--- includes/settings/layouts/tjg-csbs-admin-display.php
+++ includes/settings/layouts/tjg-csbs-admin-display.php
@@ -208,7 +208,7 @@ $common = new Common();
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" type="submit" form="updateCandidate" value="Submit">Save</button>
+                        <button type="submit" class="btn btn-primary" form="updateCandidate" value="Submit">Save</button>
                     </div>
                 </form>
             </div>
